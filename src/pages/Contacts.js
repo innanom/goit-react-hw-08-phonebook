4471cd5d8d7d80ff561fd98df9504fd086ff2f97
Inna Nomerovkina <inkosof@gmail.com>
2023-06-29 17:16:19 +0300

    Pages

---
+++ src/pages/Contacts.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { ContactsList } from 'components';
+import { ContactsForm } from 'components/';
+import { fetchContacts } from 'redux/Contacts/operations';
+import { selectLoading } from 'redux/tasks/selectors';
+
+export default function Tasks() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Helmet>
+        <title>Your contacts</title>
+      </Helmet>
+      <ContactsForm/>
+      <div>{isLoading && 'Request in progress...'}</div>
+      <ContactsList />
+    </>
+  );
+}
---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+`;
+
+export const Title = styled.h1`
+  fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+`;
+
+
