7c272b4e7cecee3021d0f30ee98eac64b32adafd
Inna Nomerovkina <inkosof@gmail.com>
2023-06-30 16:00:36 +0300

    fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -12,7 +12,7 @@ export const Navigation = () => {
         Home
       </NavLink>
       {isLoggedIn && (
-        <NavLink  to="/tasks">
+        <NavLink  to="/contacts">
           Contacts
         </NavLink>
       )}
